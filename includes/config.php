ae8f2583a65a25cbc214b6fa07559a53753905ff
ivan muliarchyk <ya-gans@mail.ru>
2017-12-24 20:47:07 +0300

    Connect database

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php 
+	ob_start();
+
+	$timezon = date_default_timezone_set("Europe/Minsk");
+
+	$con = mysqli_connect("localhost", "root", "", "slotify");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+?>
